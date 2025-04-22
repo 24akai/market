b45a4be08f4e6f782bf81994e1dea2d4ef5b0630
24akai <24a_kai@.liceum.ztu.edu.ua>
2025-04-22 13:03:29 +0300

    Upgraded list of products

--- js/script.js
+++ js/script.js
@@ -31,7 +31,6 @@ console.log('Перевірка підключеного файлу скрипт
 //}
 
 
-
 async function getObjectsFromFile(file) {
   try {
     const response = await fetch(file);
@@ -46,9 +45,52 @@ async function getObjectsFromFile(file) {
     console.log('Fetch завершено!');
   }
 }
+async function buildItems(){
+  const itemsData = await getObjectsFromFile('/items.json');
+  console.log(itemsData);
 
+  if (!itemsData) {
+    console.error('Відсутні дані у JSON-файлі!');
+    return;
+  }
+
+  const itemsDiv = document.getElementById("items");
+
+  itemsData.forEach((item, index) => {
+    const itemDiv = document.createElement('div');
+    itemDiv.classList.add("item");
+
+    itemDiv.innerHTML = `
+    <div class="item">
+  <div class="item-title">${item.title}</div>
+
+  <div class="item-image">
+    <img src="img/${item.photo}" alt="${item.title}" class="motokosa-img" />
+  </div>
+
+  <div class="parts-pay">
+    <div><img src="img/mono-lapka.png" alt="" />${item.paw}</div>
+    <div><img src="img/pb.png" alt="" />${item.pb}</div>
+  </div>
+
+  <div class="price">
+    <div><span>${item.standart_price}</span><sup>грн</sup></div>
+    <div><span>${item.price}</span><sup>грн</sup></div>
+  </div>
+
+  <div class="price bonus">
+    ціна за купоном
+    <div><span>${item.bonus_price}</span><sup>грн</sup></div>
+  </div>
+</div>
+    `
+
+    itemsDiv.appendChild(itemDiv);
+  });
+}
 
 buildItems();
+
 
 /*if (itemsDiv) {
     itemsArray.forEach((item) => {
